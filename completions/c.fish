3fec0147b44fdb3fc54d8ffd3fd0e39fe5016e60
Chris Castle <github@crc.io>
2021-09-04 16:42:17 -0700

    Change Code directory structure
    Now expects Code to contain a directory tree of, for example
    github.com/crcastle/my-cool-project
    
    github.com and gitlab.com are currently supported

--- completions/c.fish
+++ completions/c.fish
@@ -1,1 +1,1 @@
-complete --command c --wraps="cd" --arguments '(__fish_complete_directories ~/Code/ | sed -e "s|$HOME/Code/||")'
+complete --exclusive --command c --arguments '(find ~/Code/{github.com,gitlab.com} -type d -maxdepth 2 -mindepth 2 -exec echo {}/\tDirectory \; | sed -e "s|$HOME/Code/||")'
